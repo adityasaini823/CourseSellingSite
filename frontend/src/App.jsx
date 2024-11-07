6e5e9378261f66f78c2beea8113967dbd43525f6
Aditya Saini <102839228+adityasaini823@users.noreply.github.com>
2024-11-07 18:12:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import {Routes, Route} from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import InstructorDashBoard from "./pages/IntructorDashBoard";
 function App() {
 
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/home" element={<HomePage/>}/>
+      <Route path="/instructor" element={<InstructorDashBoard />} />
      </Routes>
  
   )
